92773b94d58b92b977c7cb86af0a43b9a3bc3542
Guillaume Pitot <gpitot@nine.com.au>
2019-04-03 10:36:56 +1100

    smooth scroll

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import 'whatwg-fetch'
 import ArrowClicks from './js/gallery/index';
 import {DisplaySection} from './js/upload/index';
 import getData from './js/stream/index';
+import smoothscroll from 'smoothscroll-polyfill';
 
+// kick off the polyfill!
+smoothscroll.polyfill();
 
 ArrowClicks();
 getData();
